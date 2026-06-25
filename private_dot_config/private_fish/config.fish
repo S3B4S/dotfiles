71b210c112c242df5de9cfab43937ccc5cacc445
Kevin van der Werff <kevin@polars.tech>
2026-06-25 23:00:00 +0200

    init

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+# Commands to run in interactive sessions can go here
+end
+
+alias dev="cd ~/Development"
+alias p="cd ~/Development/polars-cloud"
+alias pf="cd ~/Development/polars-cloud/frontend"
+alias l="eza -la"
+alias docker="orb docker"
