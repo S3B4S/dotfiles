95ef92f3a070690d08d60b091a61cf0dcac94042
Kevin van der Werff <kevin@polars.tech>
2026-06-29 00:22:00 +0200

    add EDITOR & disable CMD M

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -2,6 +2,8 @@ if status is-interactive
 # Commands to run in interactive sessions can go here
 end
 
+set -gx EDITOR 'code --wait'
+
 alias dev="cd ~/Development"
 alias p="cd ~/Development/polars-cloud"
 alias pf="cd ~/Development/polars-cloud/frontend"
