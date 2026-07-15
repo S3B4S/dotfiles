451c81e6ed46ded33456eab24146ae10b2780f4a
Kevin van der Werff <kevin@polars.tech>
2026-07-15 10:08:11 +0200

    add biome formatting

--- private_dot_config/nvim/lua/configs/conform.lua
+++ private_dot_config/nvim/lua/configs/conform.lua
@@ -1,15 +1,20 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    javascript = { "biome-check" },
+    javascriptreact = { "biome-check" },
+    typescript = { "biome-check" },
+    typescriptreact = { "biome-check" },
+    json = { "biome-check" },
+    jsonc = { "biome-check" },
+    css = { "biome-check" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
 
 return options
