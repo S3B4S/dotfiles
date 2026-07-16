487e801f97dd6c46f9af0f20818f82b08ad07ca8
Kevin van der Werff <kevin@polars.tech>
2026-07-17 01:30:00 +0200

    Add yoink file path relative to cmd root

--- private_dot_config/nvim/lua/mappings.lua
+++ private_dot_config/nvim/lua/mappings.lua
@@ -8,3 +8,10 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+map("n", "<leader>yp", function()
+	local root = (vim.fs.root(0, ".git") or vim.uv.cwd()) .. "/"
+	local abs = vim.api.nvim_buf_get_name(0)
+	local rel = (abs:sub(1, #root) == root) and abs:sub(#root + 1) or abs
+	vim.fn.setreg("+", rel)
+end, { desc = "yank relative path" })
