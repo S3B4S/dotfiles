86c812728246f8a085cd08a2c6e4509fb559b2b0
Kevin van der Werff <kevin@polars.tech>
2026-07-17 15:48:14 +0200

    remove I workspace aerospace, highlight buffers for open items in nvimtree

--- private_dot_config/nvim/lua/configs/nvimtree.lua
+++ private_dot_config/nvim/lua/configs/nvimtree.lua
@@ -2,6 +2,10 @@ local M = {
 	view = {
 		adaptive_size = true,
 	},
+	renderer = {
+		highlight_opened_files = "all",
+		special_files = { "Cargo.toml", "Makefile" }, -- drop README.md
+	},
 }
 
 return M
