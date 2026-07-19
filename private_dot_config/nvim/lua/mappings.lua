1b0fdbf3781d7ef2023c2e5aabc4dcb20a80e021
Kevin van der Werff <kevin@polars.tech>
2026-07-19 13:54:02 +0200

    Telescope show diagnostics

--- private_dot_config/nvim/lua/mappings.lua
+++ private_dot_config/nvim/lua/mappings.lua
@@ -23,3 +23,10 @@ end, { buffer = bufnr, desc = "telescope lsp references" })
 vim.keymap.del("n", "<C-w>d")
 vim.keymap.del("n", "<C-w><C-d>")
 map("n", "<leader>dd", vim.diagnostic.open_float, { desc = "show diagnostic" })
+
+-- delete nvchad keymap
+-- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvchad/mappings.lua#L29
+vim.keymap.del("n", "<leader>ds")
+map("n", "<leader>ds", function()
+	require("telescope.builtin").diagnostics({ bufnr = 0, initial_mode = "normal" })
+end, { desc = "telescope diagnostics" })
