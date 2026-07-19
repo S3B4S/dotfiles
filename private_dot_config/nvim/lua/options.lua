5c55d54d5f537e3f8c830c631c7d5305f7c0292b
Kevin van der Werff <kevin@polars.tech>
2026-07-19 13:11:40 +0200

    Close any existing float so it also closes floats opened through other ways

--- private_dot_config/nvim/lua/options.lua
+++ private_dot_config/nvim/lua/options.lua
@@ -7,14 +7,18 @@ o.cursorlineopt = "both" -- to enable cursorline!
 
 vim.opt.relativenumber = true
 
-local diag_float_winid = nil
+local function close_existing_float(bufnr)
+	local winid = vim.b[bufnr].lsp_floating_preview
+	if winid and vim.api.nvim_win_is_valid(winid) then
+		vim.api.nvim_win_close(winid, true)
+	end
+end
+
 vim.diagnostic.config({
 	jump = {
 		on_jump = function(_, bufnr)
-			if diag_float_winid and vim.api.nvim_win_is_valid(diag_float_winid) then
-				vim.api.nvim_win_close(diag_float_winid, true)
-			end
-			_, diag_float_winid = vim.diagnostic.open_float()
+			close_existing_float(bufnr)
+			vim.diagnostic.open_float()
 		end,
 	},
 })
