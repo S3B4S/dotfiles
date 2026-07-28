868de90f2836cd04c513af2d9d04b096b402e844
Kevin van der Werff <kevin@polars.tech>
2026-07-28 22:16:04 +0200

    <C-j/k> for navigation

--- private_dot_config/nvim/lua/configs/telescope.lua
+++ private_dot_config/nvim/lua/configs/telescope.lua
@@ -1,5 +1,15 @@
 return {
 	defaults = {
 		path_display = { shorten = 5, "filename_first" },
+		mappings = {
+			i = {
+				["<C-j>"] = function(...)
+					return require("telescope.actions").move_selection_next(...)
+				end,
+				["<C-k>"] = function(...)
+					return require("telescope.actions").move_selection_previous(...)
+				end,
+			},
+		},
 	},
 }
--- private_dot_config/nvim/lua/plugins/init.lua
+++ private_dot_config/nvim/lua/plugins/init.lua
@@ -18,6 +18,16 @@ return {
 	},
 
 	{
+		"hrsh7th/nvim-cmp",
+		opts = function(_, opts)
+			local cmp = require("cmp")
+			opts.mapping["<C-j>"] = cmp.mapping.select_next_item()
+			opts.mapping["<C-k>"] = cmp.mapping.select_prev_item()
+			return opts
+		end,
+	},
+
+	{
 		"nvim-tree/nvim-tree.lua",
 		opts = require("configs.nvimtree"),
 	},
